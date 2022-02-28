84c629532785c1bf5d12b0b754839ede4d265542
nick <kamarinskiv@mail.ru>
2022-02-28 19:07:13 +0700

    измнил view

--- admin/main/views.py
+++ admin/main/views.py
@@ -8,28 +8,8 @@ from admin.main.models import BotUsers
 @csrf_exempt
 def process_pay_notification(request):
     if request.method == 'POST':
-        print("request")
-        data = json.loads(request.body)
-        with open('request_data', 'a', encoding='utf-8') as file:
-            json.dump(data, file)
-            file.write('\n')
-        # print(request)
-        # try:
-        #     with open("request_data.txt") as f:
-        #         f.write(request.POST)
-        # except Exception as e:
-        #     f.write(str(e))
-        # try:
-        #     with open("request_data.txt", "a") as f:
-        #         f.write(request.POST)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     with open("request_data.txt", "a") as f:
-        #         f.write(request)
-        # except Exception as e:
-        #     print(e)
-        # print(request)
+        with open('request_data.txt', 'w', encoding='utf-8') as file:
+            print(request, file=file)
         return HttpResponse({"code": 0})
     else:
         return HttpResponse("get request")
