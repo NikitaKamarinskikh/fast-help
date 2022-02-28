c8204514aa091727d77ca686bf85fb321c30036a
nick <kamarinskiv@mail.ru>
2022-02-28 19:10:33 +0700

    измнил view

--- admin/main/views.py
+++ admin/main/views.py
@@ -10,6 +10,8 @@ def process_pay_notification(request):
     if request.method == 'POST':
         with open('request_data.txt', 'w', encoding='utf-8') as file:
             print(request, file=file)
+            print(request.POST, file=file)
+            print(request.POST.__dict__, file=file)
         return HttpResponse({"code": 0})
     else:
         return HttpResponse("get request")
