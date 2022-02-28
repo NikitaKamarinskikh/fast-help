7100fcbaa493bf7b6a544dbedc6238e7a3991273
nick <kamarinskiv@mail.ru>
2022-02-28 17:47:42 +0700

    Изменил views

--- admin/main/views.py
+++ admin/main/views.py
@@ -7,8 +7,11 @@ from admin.main.models import BotUsers
 def process_pay_notification(request):
     if request.method == 'POST':
         print(request)
-        with open("request_data.txt", "w") as f:
-            f.write(request.text)
+        try:
+            with open("request_data.txt", "w") as f:
+                f.write(request.text)
+        except:
+            ...
         print(request)
         return HttpResponse({"code": 0})
     else:
