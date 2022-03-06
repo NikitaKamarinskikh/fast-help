02f60d35c57f81b11d2c915c3fe4b87e85ead304
nick <kamarinskiv@mail.ru>
2022-03-06 16:05:04 +0700

    Изменил view

--- admin/main/views.py
+++ admin/main/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseNotFound
 from keyboards.inline.send_order_to_workers import send_order_to_workers_markup
+from keyboards.default.main import main_markup
 from admin.main.models import BotUsers
 from admin.orders.models import Orders
 from admin.transactions.models import Transactions
@@ -21,8 +22,8 @@ def set_order_waiting_for_start_status(order_id: int):
     order.save()
 
 
-def count_bonus(sum_: int):
-    return round(REFERRER_BONUS_PERCENT * (1/100) * sum_)
+def count_bonus(sum_: float):
+    return round(REFERRER_BONUS_PERCENT * (1 / 100) * sum_)
 
 
 def get_user_by_id(user_id: int):
@@ -39,16 +40,20 @@ def send_message(user_telegram_id):
     r = post(url)
 
 
-def notify_user_about_success_transaction(user_telegram_id: int, text: str, reply_markup=None):
-    if reply_markup:
-        url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' \
-              f'{user_telegram_id}&text={text}&reply_markup={reply_markup}'
-    else:
-        url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' \
-              f'{user_telegram_id}&text={text}'
-    r = post(url)
+def notify_user_about_success_transaction(user_telegram_id: int, new_user_coins: int, reply_markup=None):
+    success_transaction_url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' \
+                              f'{user_telegram_id}&text=Оплата принята\nТекущее количество монет: {new_user_coins}&' \
+                              f'reply_markup={main_markup}'
+    r = post(success_transaction_url)
     response = json.loads(r.content.decode('utf-8'))
     print(response)
+    if reply_markup:
+        url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' \
+              f'{user_telegram_id}&text=&reply_markup={reply_markup}'
+
+        r = post(url)
+        response = json.loads(r.content.decode('utf-8'))
+        print(response)
 
 
 def notify_referrer(referrer_telegram_id: int, bonus: int, new_referrer_balance: int):
@@ -76,23 +81,20 @@ def process_pay_notification(request):
 
             transaction = get_transaction_by_id(transaction_id)
             reply_markup = None
-
+            new_user_coins = user.coins
             if not transaction.is_paid:
                 transaction.is_paid = True
                 transaction.save()
-                text = "Оплата принята"
                 if has_order:
                     reply_markup = send_order_to_workers_markup(order_id, distance)
                     set_order_waiting_for_start_status(order_id)
-                    text += "\nЧобы отправить задание исполнителям, нажмите на прикрепленную кнопку"
                 else:
                     current_user_coins = user.coins
                     new_user_coins = current_user_coins + coins
                     user.coins = new_user_coins
                     user.save()
-                    text += f"\nВаш баланс {new_user_coins} монет"
 
-                notify_user_about_success_transaction(user.telegram_id, text, reply_markup)
+                notify_user_about_success_transaction(user.telegram_id, new_user_coins, reply_markup)
 
                 if user.referrer:
                     bonus = count_bonus(amount)
@@ -131,4 +133,3 @@ reques.post = <QueryDict: {'TransactionId': ['1037510691'], 'Amount': ['1.00'], 
 'TotalFee': ['0.00'], 'CardProduct': ['TNW'], 'PaymentMethod': ['']}>
 {'_encoding': 'utf-8', '_mutable': False}
 """
-
