e98b4258b294d2eab8734be47c2066e8ea62897f
nick <kamarinskiv@mail.ru>
2022-03-09 11:40:18 +0700

    изменил view

--- admin/main/views.py
+++ admin/main/views.py
@@ -49,14 +49,14 @@ def notify_user_about_success_transaction(user_telegram_id: int, new_user_coins:
                               f'reply_markup={main_markup}'
     r = post(success_transaction_url)
     response = json.loads(r.content.decode('utf-8'))
-    logging.debug(response)
+    logging.info(response)
     if reply_markup:
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=' \
               f'{user_telegram_id}&text=Чтобы отправить уведомление исполнителям, нажмите на прикрепленную кнопку&' \
               f'reply_markup={reply_markup}'
         r = post(url)
         response = json.loads(r.content.decode('utf-8'))
-        logging.debug(response)
+        logging.info(response)
 
 
 def notify_referrer(referrer_telegram_id: int, bonus: int, new_referrer_balance: int):
@@ -79,7 +79,7 @@ def process_pay_notification(request):
             order_id = int(data.get("order_id"))
             has_order = data.get("has_order")
             coins = int(data.get("coins"))
-            distance = data.get("distance")
+            distance = int(data.get("distance"))
             with_bonus = data.get("with_bonus")
 
             transaction = get_transaction_by_id(transaction_id)
@@ -90,6 +90,10 @@ def process_pay_notification(request):
                 transaction.save()
                 if has_order:
                     if with_bonus:
+                        if distance == 500:
+                            coins -= 30
+                        if distance == 1000:
+                            coins -= 50
                         current_user_coins = user.coins
                         new_user_coins = current_user_coins + coins
                         user.coins = new_user_coins
