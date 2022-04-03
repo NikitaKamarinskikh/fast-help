b492f34ded0a7ba8accaa6cf786d05ef9f596fb0
nick <kamarinskiv@mail.ru>
2022-04-03 20:02:09 +0700

    Отключил обработку хендлеров для разработки

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,7 @@
 from .errors import dp
 from .cancel import dp
 from .admin import dp
-from .dev import dp
+# from .dev import dp
 from .payments import dp
 from .customers import dp
 from .workers import dp
